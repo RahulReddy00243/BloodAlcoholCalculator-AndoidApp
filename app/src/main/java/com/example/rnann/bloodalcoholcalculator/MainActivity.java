1a2339044a618e8a60d7823b29bdd36577fb2966
RahulReddyNannuru <rnannuru@gmail.com>
2016-11-17 03:35:51 -0500

    done With UI!!

--- app/src/main/java/com/example/rnann/bloodalcoholcalculator/MainActivity.java
+++ app/src/main/java/com/example/rnann/bloodalcoholcalculator/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.rnann.bloodalcoholcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText weightET;
+    Switch aSwitch;
+    String weight;
+    String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+    }
+
+    private void getWandG(){
+
+        weightET = (EditText) findViewById(R.id.WeightET);
+        aSwitch = (Switch) findViewById(R.id.Switch);
+
+        weight = weightET.getText().toString();
+        
+
     }
 }
